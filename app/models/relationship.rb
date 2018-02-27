eb1b32c2e4efb3cedd6b5981b1e5e93f2b65a726
Claudine Pradal <claudinecartailler@gmail.com>
2018-02-27 12:37:50 +0100

    routes done and checked by Jojo

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -7,4 +7,10 @@ class Relationship < ApplicationRecord
   validates :first_guest, presence: true
   validates :second_guest, presence: true
   enum link: { couple: 0, hate: 1 }
+
+  # after_create :recipro
+
+  # def recipro
+  #   Relationship.create(first_guest: self.second_guest ,second_guest: first_guest)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :seating_plans, only: [:create ] do
+    resources :tables, only: [:index]
+
+  end
+
+  resources :tables, only: [] do
+    resources :participants, only: [:create, :update, :destroy], shallow: true
+  end
+
+  resources :relationships, only: [:create]
+
+  patch 'participants/swap'
+  put 'participants/swap'
+
 end
