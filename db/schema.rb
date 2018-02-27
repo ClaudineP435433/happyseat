a8f7cb6af025e84a5596b15d47d9fa940ca6688d
Victor Sassonia <victor.sassonia@gmail.com>
2018-02-27 15:39:52 +0100

    add columns seating plan tables + create seating plan controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226161446) do
+ActiveRecord::Schema.define(version: 20180227142237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20180226161446) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "nb_tables"
+    t.integer "nb_max_participants"
     t.index ["user_id"], name: "index_seating_plans_on_user_id"
   end
 
---
+++ test/controllers/seating_plans_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SeatingPlansControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
