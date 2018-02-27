4dfa0b0bb706e6b526f56cb69804bc27cb45213d
Victor Sassonia <victor.sassonia@gmail.com>
2018-02-27 17:33:41 +0100

    css et simple form home

--- app/controllers/seating_plans_controller.rb
+++ app/controllers/seating_plans_controller.rb
@@ -2,7 +2,15 @@ class SeatingPlansController < ApplicationController
 
   def create
     @seating_plan = current_user.seating_plans.new(seating_plan_params)
+    @seating_plan.default_name
+
+    n = 1
+
     if @seating_plan.save
+      params[:seating_plan][:nb_tables].to_i.times do
+        @seating_plan.tables.create(name: "Table #{n}", nb_max_participants: params[:seating_plan][:nb_max_participants])
+        n += 1
+      end
       redirect_to root_path
     else
       render "pages/new"
