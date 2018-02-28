d0c528a5daaf94140d6e97e9f51c10cdfa124bfd
Claudine Pradal <claudinecartailler@gmail.com>
2018-02-28 17:44:05 +0100

    Add of modal Relationship

--- app/javascript/components/toolbar.js
+++ app/javascript/components/toolbar.js
@@ -1,11 +1,16 @@
 function hiddenCouple() {
   const toolbarFormCouple = document.querySelector('.toolbar-form-couple');
   const toolbarFormSingle = document.querySelector('.toolbar-form-one-participant');
-  const button = document.querySelector('.active-couple');
+  const buttons = document.querySelectorAll('.active-couple');
 
-  button.addEventListener('click', () => {
-      toolbarFormCouple.classList.remove('hidden-couple');
-      toolbarFormSingle.classList.add('hidden-couple');
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      toolbarFormCouple.classList.toggle('hidden-couple');
+      toolbarFormSingle.classList.toggle('hidden-couple');
+      buttons.forEach((b) => {
+        b.classList.toggle('hidden-couple');
+      });
+    });
   });
 }
 
