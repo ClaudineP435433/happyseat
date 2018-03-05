85f438e8a5c94abb960bea451d767622efbee027
Victor Sassonia <victor.sassonia@gmail.com>
2018-03-05 15:20:38 +0100

    drugular with swap

--- app/javascript/components/dragula.js
+++ app/javascript/components/dragula.js
@@ -1,21 +1,59 @@
 
 import dragula from 'dragula';
-let index = 0
-let list = []
-
-document.querySelectorAll('.table-card').forEach((table) => {
-  let table_nb_top = document.querySelector(`#table-top-${index}`);
-  list.push(table_nb_top);
-  let table_nb_bottom = document.querySelector(`#table-bottom-${index}`);
-  list.push(table_nb_bottom);
-  index += 1;
- });
-
+let seatIndex = 0
 
 const area = document.querySelector('#drop-zone');
-list.push(area);
 
+let containers = [area]
 
-dragula(list);
+const dragDecorator = (containers, options, transitZone) => {
+  const dragulaConst = dragula( containers, options);
+  dragulaConst.on('drop', (el, target, source, sibling) => {
+    if (transitZone !== target && target.children.length > 1) {
+      const element = sibling ? sibling : el;
+      element.classList.remove("gu-transit");
+      source.insertAdjacentHTML("beforeend", element.outerHTML);
+      element.remove();
+    }
+  });
+};
+
+document.querySelectorAll('.table-card').forEach((table) => {
+  let table_nb_top = document.querySelector(`#table-top-${seatIndex}`);
+  containers.push(table_nb_top);
+  let table_nb_bottom = document.querySelector(`#table-bottom-${seatIndex}`);
+  containers.push(table_nb_bottom);
+  seatIndex += 1;
+ });
+
+const options = { };
+dragDecorator(containers, options);
 
 import 'dragula/dist/dragula.css';
+
+
+// -----------------
+
+// const dragDecorator = (containers, options, transitZone) => {
+//   const dragulaConst = dragula( containers, options);
+//   dragulaConst.on('drop', (el, target, source, sibling) => {
+//     if (transitZone !== target && target.children.length > 1) {
+//       const element = sibling ? sibling : el;
+//       element.classList.remove("gu-transit")
+//       source.insertAdjacentHTML("beforeend", element.outerHTML);
+//       element.remove();
+//     }
+//   });
+// };
+
+// const top = document.querySelector('#top');
+// const containers = [
+//   top,
+//   document.querySelector('#left'),
+//   document.querySelector('#center-left'),
+//   document.querySelector('#center-right'),
+//   document.querySelector('#right')
+// ];
+// const options = { };
+// dragDecorator(containers, options, top)
+
