856ac55d71574f256d8967f62364d07c9044bac7
Claudine Pradal <claudinecartailler@gmail.com>
2018-03-06 10:53:43 +0100

    Color CHange according to Family Type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def avatar_colored(seat_id)
+    seat = @seating_plan.participants.find_by(seat: seat_id)
+    if seat.present?
+      if seat.family_type == "bride"
+        "avatar_pink"
+      elsif seat.family_type == "groom"
+        "avatar_blue"
+      else
+        "avatar_color"
+      end
+    end
+  end
 end
