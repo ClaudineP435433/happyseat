a700b63b0a7a80c343aaa99ebe012432f544084e
Claudine Pradal <claudinecartailler@gmail.com>
2018-04-19 14:13:53 +0200

    implement smooth transition

--- app/javascript/components/toolbar_guest.js
+++ app/javascript/components/toolbar_guest.js
@@ -4,17 +4,17 @@ const chevron = document.getElementById('chevron');
 
 function displayGuest() {
   chevron.addEventListener('click', () => {
-    toolbarGuest.classList.remove('hidden-couple');
+    //toolbarGuest.classList.remove('hidden-couple');
+    toolbarGuest.classList.toggle('toolbar-guests-view');
     chevron.classList.add('hidden-couple');
   });
 }
 
 function hiddenGuest() {
   toolbarGuestClose.addEventListener('click', () => {
-    toolbarGuest.classList.add('hidden-couple');
+    toolbarGuest.classList.toggle('toolbar-guests-view');
     chevron.classList.remove('hidden-couple');
   });
 }
-
 
 export { displayGuest, hiddenGuest };
